bcf62adf71fef855dc35873f4831a90fd67006ad
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-05-14 14:50:15 +0700

    Add event to navigate to detail activity.

--- app/src/main/java/com/github/fajaragungpramana/intent/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/intent/ui/main/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.github.fajaragungpramana.intent.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import com.github.fajaragungpramana.intent.common.app.AppActivity
 import com.github.fajaragungpramana.intent.databinding.ActivityMainBinding
+import com.github.fajaragungpramana.intent.ui.detail.DetailActivity
 
 class MainActivity : AppActivity<ActivityMainBinding>() {
 
     override fun onViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     override fun onCreated(savedInstanceState: Bundle?) {
+
+        viewBinding.mbDetail.setOnClickListener {
+            val intent = Intent(this@MainActivity, DetailActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
